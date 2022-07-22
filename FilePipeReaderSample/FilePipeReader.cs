757c72cc05208b4ae3c98acf0e6e90a9fd369147
HideyukiKitao <panda728z@gmail.com>
2022-07-22 22:48:08 +0900

    プロジェクト ファイルを追加します。

---
+++ FilePipeReaderSample/FilePipeReader.cs
@@ -0,0 +1,69 @@
+﻿using System.Buffers;
+using System.IO.Pipelines;
+
+namespace FilePipeReaderSample
+{
+    public class FilePipeReader
+    {
+        public delegate void ReadTo(in ReadOnlySpan<byte> buffer);
+
+        public static async Task ReadAllAsync(Stream input, Memory<byte> delimiter, ReadTo readTo)
+        {
+            var pipe = new Pipe();
+            var writing = FillPipeAsync(input, pipe.Writer);
+            var reading = ReadPipeAsync(pipe.Reader, delimiter, readTo);
+            await Task.WhenAll(writing, reading);
+        }
+
+        static async Task FillPipeAsync(Stream input, PipeWriter writer)
+        {
+            while (true)
+            {
+                try
+                {
+                    var memory = writer.GetMemory();
+                    int byteRead = await input.ReadAsync(memory);
+                    if (byteRead == 0)
+                        break;
+                    writer.Advance(byteRead);
+                }
+                catch
+                {
+                    break;
+                }
+                var result = await writer.FlushAsync();
+                if (result.IsCompleted)
+                    break;
+            }
+            writer.Complete();
+        }
+
+        static async Task ReadPipeAsync(PipeReader reader, Memory<byte> delimiter, ReadTo readTo)
+        {
+            while (true)
+            {
+                var result = await reader.ReadAsync();
+                var buffer = result.Buffer;
+                ReadBuffer(ref buffer, delimiter, readTo);
+                reader.AdvanceTo(buffer.Start, buffer.End);
+                if (result.IsCompleted)
+                    break;
+            }
+            reader.Complete();
+        }
+
+        static void ReadBuffer(ref ReadOnlySequence<byte> buffer, Memory<byte> delimiter, ReadTo readTo)
+        {
+            var sequenceReader = new SequenceReader<byte>(buffer);
+            while (!sequenceReader.End)
+            {
+                while (sequenceReader.TryReadTo(out ReadOnlySpan<byte> line, delimiter.Span))
+                    readTo(line);
+
+                buffer = buffer.Slice(sequenceReader.Position);
+                sequenceReader.Advance(buffer.Length);
+            }
+        }
+    }
+
+}
---
+++ FilePipeReaderSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using FilePipeReaderSample;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+var enc = Encoding.GetEncoding("shift-jis");
+
+using var stream = File.Open("data01.dat", FileMode.Open);
+var row = 0;
+await FilePipeReader.ReadAllAsync(
+    stream,
+    new byte[] { (byte)'\r', (byte)'\n' },
+    (in ReadOnlySpan<byte> buffer) =>
+    {
+        Console.WriteLine($"{++row:00000}");
+        Console.WriteLine(enc.GetString(buffer));
+    });
